be875c1f3a6db9d22809a41e6e02ac989109e178
Sibahle <mayekisosibahle@gmail.com>
2023-03-15 16:44:57 +0200

    Created Test cases for UpdateScore method

--- src/ScoresSystem.spec.ts
+++ src/ScoresSystem.spec.ts
@@ -1,10 +1,73 @@
 import { GameScoreSystem } from "./ScoreSystem";
 
-const scoreSystem = new GameScoreSystem(10, 0);
-
 describe("UpdateScore", () => {
+    test("should return 1 given UpdateScore(0,1)", () => {
+        //Arrange
+        const scoreSystem = new GameScoreSystem(10, 0);
+        const expectedResult = 1;
+        const bucketNumber = 1;
+        const currentScore = 0;
 
-    test("should return 1 given UpdateScore(0,3)", () => {
-        expect(scoreSystem.UpdateScore(scoreSystem.GetCurrentScore(), 3)).toBe(1);
-    })
+        //Act
+        const actualResult = scoreSystem.UpdateScore(currentScore, bucketNumber)
+        
+        //Assert
+        expect(actualResult).toEqual(expectedResult);
+    });
+
+    test("should return 2 given UpdateScore(0,2)", () => {
+        //Arrange
+        const scoreSystem = new GameScoreSystem(10, 0);
+        const expectedResult = 2;
+        const bucketNumber = 2;
+        const currentScore = 0;
+
+        //Act
+        const actualResult = scoreSystem.UpdateScore(currentScore, bucketNumber)
+        
+        //Assert
+        expect(actualResult).toEqual(expectedResult);
+    });
+
+    test("should return 3 given UpdateScore(0,3)", () => {
+        //Arrange
+        const scoreSystem = new GameScoreSystem(10, 0);
+        const expectedResult = 3;
+        const bucketNumber = 3;
+        const currentScore = 0;
+
+        //Act
+        const actualResult = scoreSystem.UpdateScore(currentScore, bucketNumber)
+        
+        //Assert
+        expect(actualResult).toEqual(expectedResult);
+    });
+
+    test("should return 4 given UpdateScore(0,4)", () => {
+        //Arrange
+        const scoreSystem = new GameScoreSystem(10, 0);
+        const expectedResult = 4;
+        const bucketNumber = 4;
+        const currentScore = 0;
+
+        //Act
+        const actualResult = scoreSystem.UpdateScore(currentScore, bucketNumber)
+        
+        //Assert
+        expect(actualResult).toEqual(expectedResult);
+    });
+
+    test("should return 5 given UpdateScore(0,5)", () => {
+        //Arrange
+        const scoreSystem = new GameScoreSystem(10, 0);
+        const expectedResult = 5;
+        const bucketNumber = 5;
+        const currentScore = 0;
+
+        //Act
+        const actualResult = scoreSystem.UpdateScore(currentScore, bucketNumber)
+        
+        //Assert
+        expect(actualResult).toEqual(expectedResult);
+    });
 })
